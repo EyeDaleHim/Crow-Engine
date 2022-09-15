c6cdf7e7e2cfdae81c81676240add916a71bf326
Flap <weldedflap@gmail.com>
2022-09-14 20:36:03 -0400

    some stuff
    so you don't get stuck in freeplay lmao

--- source/states/menus/FreeplayState.hx
+++ source/states/menus/FreeplayState.hx
@@ -1,8 +1,20 @@
 package states.menus;
 
 import flixel.FlxG;
+import states.menus.MainMenuState;
 
 class FreeplayState extends MusicBeatState
 {
-    
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		if (FlxG.keys.justPressed.ESCAPE || FlxG.keys.justPressed.BACKSPACE)
+			FlxG.switchState(new MainMenuState());
+
+		super.update(elapsed);
+	}
 }
