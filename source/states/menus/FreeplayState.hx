ea4e9f05ae205a9a1cdb828d26264bc759ed4391
Flap <weldedflap@gmail.com>
2022-09-17 00:03:54 -0400

    add freeplay bg
    thats all, just the freeplay bg

--- source/states/menus/FreeplayState.hx
+++ source/states/menus/FreeplayState.hx
@@ -6,8 +6,17 @@ import states.menus.MainMenuState;
 
 class FreeplayState extends MusicBeatState
 {
+	public var background:FlxSprite;
+
 	override public function create()
 	{
+		background = new FlxSprite(0, 0).loadGraphic(Paths.image("menus/freeplayBG"));
+		background.scale.set(1.1, 1.1);
+		background.screenCenter();
+		background.scrollFactor.set();
+		background.antialiasing = Settings.getPref('antialiasing', true);
+		add(background);
+
 		super.create();
 	}
 
