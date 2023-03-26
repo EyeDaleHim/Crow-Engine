81661730b1f8380af63306d2e7ede12eaad5eb2c
EyeDaleHim <71162374+EyeDaleHim@users.noreply.github.com>
2023-03-26 21:20:57 +0800

    forgot these

--- source/objects/ui/Alphabet.hx
+++ source/objects/ui/Alphabet.hx
@@ -1,4 +1,4 @@
-package objects;
+package objects.ui;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
--- source/objects/ui/WeekSprite.hx
+++ source/objects/ui/WeekSprite.hx
@@ -1,4 +1,4 @@
-package objects;
+package objects.ui;
 
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
