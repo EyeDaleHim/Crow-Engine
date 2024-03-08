078d72fadf33e805b975dcd63dc4f1e11cc349b5
EyeDaleHim <71162374+EyeDaleHim@users.noreply.github.com>
2024-03-08 23:15:07 +0800

    new imports

--- source/import.hx
+++ source/import.hx
@@ -9,20 +9,32 @@ import flixel.FlxSprite;
 import flixel.FlxCamera;
 
 import flixel.graphics.FlxGraphic;
+import flixel.graphics.frames.FlxFramesCollection;
+import flixel.graphics.frames.FlxAtlasFrames;
+
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
+
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
 
 import flixel.sound.FlxSound;
+import flixel.sound.FlxSoundGroup;
 
+import flixel.util.FlxColor;
 import flixel.util.FlxSignal.FlxTypedSignal;
 
 import system.Assets;
 #end
 
 import openfl.display.BitmapData;
-
+import openfl.display.BlendMode;
 import openfl.display.Sprite;
 
 import openfl.events.Event;
 import openfl.events.KeyboardEvent;
+
+import openfl.geom.ColorTransform;
 
 import openfl.media.Sound;
 
