7e9dda3fb50cf397f79ceec7c5fad5e478f4cc0e
EyeDaleHim <71162374+EyeDaleHim@users.noreply.github.com>
2025-11-01 15:17:09 +0800

    merge treenode and color into set

--- source/crow/ds/Set.hx
+++ source/crow/ds/Set.hx
@@ -455,3 +455,27 @@ class Set<T>
 		return current;
 	}
 }
+
+private enum Color
+{
+	RED;
+	BLACK;
+}
+
+private class TreeNode<T>
+{
+	public var key:T;
+	public var color:Color;
+	public var parent:TreeNode<T>;
+	public var left:TreeNode<T>;
+	public var right:TreeNode<T>;
+
+	public function new(key:T, color:Color, parent:TreeNode<T>, left:TreeNode<T>, right:TreeNode<T>)
+	{
+		this.key = key;
+		this.color = color;
+		this.parent = parent;
+		this.left = left;
+		this.right = right;
+	}
+}
