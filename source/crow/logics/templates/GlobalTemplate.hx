ac3ee67322bcc1e3759a12dfadc6dbec5f4c1132
EyeDaleHim <71162374+EyeDaleHim@users.noreply.github.com>
2025-11-15 14:05:11 +0800

    fix security vuln

--- source/crow/logics/templates/GlobalTemplate.hx
+++ source/crow/logics/templates/GlobalTemplate.hx
@@ -1,6 +1,7 @@
 package crow.logics.templates;
 
 import crow.logics.templates.Template;
+import openfl.net.URLRequest;
 
 class GlobalTemplate extends Template
 {
@@ -9,11 +10,27 @@ class GlobalTemplate extends Template
 		return [
 			"open_url" => ExecutableAction.createAction((ctx) ->
 			{
-				final url:String = ctx.values.url;
+				var url:String = ctx.values.url;
+				var target:String = ctx.values.target ?? "_blank";
 				if (url != null)
-					FlxG.openURL(url);
+				{
+					// Only allow http and https protocols for security.
+					// If no protocol is specified, default to https.
+					if (!~/(^https?:\/\/)/i.match(url))
+					{
+						url = "https://" + url;
+					}
+					else
+					{
+						trace('HTTPS only - will not open non-https url');
+					}
+					openfl.Lib.getURL(new URLRequest(url), target);
+				}
 				ctx.onComplete();
-			}, [{name: "url", type: "String", optional: false}]),
+			}, [
+					{name: "url", type: "String", optional: false},
+					{name: "target", type: "String", optional: true}
+			]),
 			"exit_game" => ExecutableAction.createAction((ctx) ->
 			{
 				final code:Int = ctx.values.code != null ? ctx.values.code : 0;
